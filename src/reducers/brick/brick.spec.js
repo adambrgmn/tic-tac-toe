62bc6f4c7f40eb5bdad6d5ed7022dc65fdf394da
Adam Bergman <adam@fransvilhelm.com>
2016-07-05 11:37:47 +0200

    Setup brick with tests

--- src/reducers/brick/brick.spec.js
+++ src/reducers/brick/brick.spec.js
@@ -4,15 +4,15 @@ import { expect } from 'chai';
 import brick from './index.js';
 
 describe('Reducer: brick', () => {
-  describe('case "OCCUPY_SPOT"', () => {
+  describe('case "PICK_SPOT_PLAYER"', () => {
     let initialState;
 
-    beforeEach(() => (initialState = List()));
+    beforeEach(() => (initialState = List().setSize(8)));
 
     it('should occupy a spot on the brick', () => {
       const stateAfter = initialState.set(1, 'x');
 
-      const action = { type: 'OCCUPY_SPOT', player: 'x', spot: 1 };
+      const action = { type: 'PICK_SPOT_PLAYER', player: 'x', spot: 1 };
 
       expect(brick(initialState, action))
         .to.equal(stateAfter);
@@ -21,10 +21,23 @@ describe('Reducer: brick', () => {
     it('should return unchanged if spot is already occupied', () => {
       const stateBefore = initialState.set(5, 'x');
       const stateAfter = initialState.set(5, 'x');
-      const action = { type: 'OCCUPY_SPOT', player: 'o', spot: 5 };
+      const action = { type: 'PICK_SPOT_PLAYER', player: 'o', spot: 5 };
 
       expect(brick(stateBefore, action))
         .to.equal(stateAfter);
+    });
+  });
+
+  describe('case "PICK_SPOT_COMPUTER"', () => {
+    let initialState;
+
+    beforeEach(() => (initialState = List().setSize(8)));
+
+    it('should return a spot picked by the computer', () => {
+      const action = { type: 'PICK_SPOT_COMPUTER', player: 'o' };
+
+      expect(brick(undefined, action))
+        .to.include('o');
     });
   });
 });
--- src/reducers/brick/index.js
+++ src/reducers/brick/index.js
@@ -1,10 +1,64 @@
+/**
+ * A reducer for brick.
+ * Brick starts with an immutable List of
+ * 9 undefined bricks. These bricks can then
+ * be occipied by either a physical player
+ * or the computer.
+ */
+
 import { List } from 'immutable';
 
-export default function brick(state = List(), action) {
+/**
+ * getRandomFreeSpot picks a random spot out
+ * of all free spots for the computer.
+ * This will be replaced by another function
+ * that determines the best move for the
+ * computer, so that it will be unbeatable.
+ *
+ * @param  {List}   state  A List of all spots
+ * @return {Number}        A random index
+ */
+const getRandomFreeSpot = (state) => {
+  // Reduce state to only contain
+  // indecies of free spots.
+  const freeSpots = state.reduce((prev, curr, i) => {
+    if (!curr) return prev.concat(i);
+    return prev;
+  }, List());
+
+  // Get a random number from 0 to
+  // the last index in the List.
+  const freeSpotsSize = freeSpots.size;
+  const randomNum = Math.floor(Math.random() * freeSpotsSize);
+
+  // Return a random index of a free spot
+  return freeSpots.get(randomNum);
+};
+
+/**
+ * brick determines the state of store-field brick.
+ * It starts with an immutable List of 9 undefined
+ * spots. The actions puts a player (x or o) on
+ * a spot.
+ *
+ * @param  {List}   state   List of spots, occupied or not
+ * @param  {Object} action  Action object, must contain type
+ * @return {List}           Returns new state
+ */
+export default function brick(state = List().setSize(8), action) {
   switch (action.type) {
-    case 'OCCUPY_SPOT':
+    case 'PICK_SPOT_PLAYER':
+      // Ignore move if the player wants to
+      // occupy a already occupied spot
       if (state.get(action.spot)) return state;
+
+      // Return state with player occupying the new spot
       return state.set(action.spot, action.player);
+
+    case 'PICK_SPOT_COMPUTER':
+      // Return state with the computer picking a random spot
+      return state.set(getRandomFreeSpot(state), action.player);
+
     default:
       return state;
   }
