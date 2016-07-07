c071efb0fa51b6b359f1fb416f8910fa6524b4b0
Adam Bergman <adam@fransvilhelm.com>
2016-07-07 16:14:42 +0200

    Updated AI-test

--- test/aiTest.js
+++ test/aiTest.js
@@ -1,7 +1,9 @@
+const argv = require('optimist').argv;
 import { List } from 'immutable';
 import testAiVsAi from './helpers/testAiVsAi';
 
-const levels = process.argv.slice(2).map(n => Number(n));
+const levels = [...argv._];
+const testRounds = argv._[2] || 100;
 
 /* eslint-disable no-console */
 const logGame = (game, played) => {
@@ -16,32 +18,32 @@ const logGame = (game, played) => {
 
   console.log(`Winner: ${game.winner}`);
   console.log(`Rounds: ${game.rounds}`);
-  console.log(`Games played: ${played} out of 100`);
+  console.log(`Games played: ${played} out of ${testRounds}`);
 };
 
 const logResult = (result) => {
   const avgRounds = result.rounds.reduce((prev, curr) => prev + curr, 0) / result.rounds.size;
-  const oWins = result.winners.filter(w => w === 'o').size;
   const xWins = result.winners.filter(w => w === 'x').size;
+  const oWins = result.winners.filter(w => w === 'o').size;
   const draws = result.winners.filter(w => w === 'draw').size;
 
   console.log('---------');
   console.log('---------');
-  console.log(`Average rounds: ${avgRounds.toFixed(3)}`);
-  console.log(`o wins: ${oWins}`);
-  console.log(`x wins: ${xWins}`);
-  console.log(`draws: ${draws}`);
+  console.log(`Average rounds: ${avgRounds.toFixed(2)}`);
+  console.log(`x wins: ${xWins} (${Math.round((xWins / testRounds) * 100)}%)`);
+  console.log(`o wins: ${oWins} (${Math.round((oWins / testRounds) * 100)}%)`);
+  console.log(`draws: ${draws} (${Math.round((draws / testRounds) * 100)}%)`);
   console.log('---------');
   console.log('Played with:');
   console.log(`  x: level ${levels[0] || 0}`);
   console.log(`  o: level ${levels[1] || 0}`);
 };
 
-const runTest = (x = 100, logProgress = true) => {
+const runTest = (logProgress = true) => {
   let rounds = List();
   let winners = List();
 
-  for (let i = 1; i <= x; i++) {
+  for (let i = 1; i <= testRounds; i++) {
     const game = testAiVsAi(...levels);
     rounds = rounds.push(game.rounds);
     winners = winners.push(game.winner);
@@ -52,5 +54,6 @@ const runTest = (x = 100, logProgress = true) => {
   return { rounds, winners };
 };
 
+console.log('The tests will now begin, x is always first');
 const result = runTest();
 logResult(result);
