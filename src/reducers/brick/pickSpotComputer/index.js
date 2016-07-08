08ff20c434bd8ef62f16e64bbd9de939ad90ea62
Adam Bergman <adam@fransvilhelm.com>
2016-07-08 10:32:37 +0200

    Made level 1 simpler

--- src/reducers/brick/pickSpotComputer/index.js
+++ src/reducers/brick/pickSpotComputer/index.js
@@ -4,12 +4,13 @@ import getLevel2Spot from './getLevel2Spot';
 import { aiLevels } from '../../../constants';
 
 export default function pickSpotComputer(state, action) {
+  const randomNum = Math.random() * 100;
+
   switch (action.level) {
     case aiLevels.zero:
       return getLevel0Spot(state);
     case aiLevels.one:
-      const randomNum = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-      if (randomNum < 5) return getLevel0Spot(state);
+      if (randomNum > 40) return getLevel0Spot(state);
       return getLevel2Spot(state, action.player);
     case aiLevels.two:
       return getLevel2Spot(state, action.player);
