269cfa34369e0f630dbb4df62d838ebcdbd139b4
Adam Bergman <adam@fransvilhelm.com>
2016-07-12 17:13:35 +0200

    UI updates

--- src/containers/GameBoardSpotContainer/index.js
+++ src/containers/GameBoardSpotContainer/index.js
@@ -1,16 +1,17 @@
 import { connect } from 'react-redux';
 
-import { pickSpotPlayer, setNextPlayer } from '../../actions';
+import { pickSpotPlayer, setNextPlayer, pickSpotComputer } from '../../actions';
 import GameBoardSpot from '../../components/GameBoard/GameBoardSpot';
 
 const mapStateToProps = (state, ownProps) => ({
   index: ownProps.index,
   player: ownProps.player,
   activePlayer: state.player,
+  level: state.aiLevel,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onSpotClick: (activePlayer) => {
+  onSpotClick: (activePlayer, level) => {
     dispatch(pickSpotPlayer(ownProps.index, activePlayer));
     dispatch(setNextPlayer());
   },
