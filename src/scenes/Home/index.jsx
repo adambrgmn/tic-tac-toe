7c64bed409af82a1cccd846a3d6ef3f077876365
Adam Bergman <adam@fransvilhelm.com>
2016-07-14 16:59:21 +0200

    Added transition between scenes

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { connect } from 'react-redux';
+
+import { screens } from '../../constants';
 
 import Menu from './scenes/Menu';
 import Game from './scenes/Game';
 
-export default function Home() {
+export function Home({ screen }) {
+  const displayMenu = screen === screens.menu ? <Menu /> : null;
+  const displayGame = screen === screens.game ? <Game /> : null;
   return (
     <div>
-      <Menu />
-      <Game />
+      <ReactCSSTransitionGroup
+        transitionName="screen-transition"
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={2000}
+      >
+        {displayMenu}
+        {displayGame}
+      </ReactCSSTransitionGroup>
     </div>
   );
 }
+
+Home.propTypes = {
+  screen: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({ screen }) => ({
+  screen,
+});
+
+export default connect(mapStateToProps)(Home);
