3791a99af872e7d206e2eba03a0e02ffa3be082c
Adam Bergman <adam@fransvilhelm.com>
2016-07-19 14:24:04 +0200

    Added react perf

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable global-require */
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
 import Home from './scenes/Home';
+
+if (process.env.NODE_ENV !== 'production') {
+  React.Perf = require('react-addons-perf');
+}
 
 render(
   <Provider store={store}>
